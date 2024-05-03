e3af8e6e6cf33262ccccae69923351fecb1bce60
David Portillo <david.portillo92@gmail.com>
2024-05-03 19:00:49 -0400

    included header placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import type { Component } from 'solid-js';
+import Header from './components/shared/Header';
 
 const App: Component = () => {
   return (
     <>
+      <Header />
       <div>HELLO</div>
     </>
   );
