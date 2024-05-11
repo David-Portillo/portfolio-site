236c91872e4525a2fdaf3a4e6bdf92ff03bcbc25
David Portillo <david.portillo92@gmail.com>
2024-05-10 23:18:32 -0400

    header placeholder

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -7,9 +7,10 @@ const Header: Component = () => {
     <div><img class={styles.logo} src="https://placehold.co/75x75" alt="logo" /></div>
     <nav>
       <ul class={styles["primary-navigation"]}>
-        <li><a href=""><span aria-hidden="true">01</span>About Me</a></li>
-        <li><a href=""><span aria-hidden="true">02</span>Skills</a></li>
-        <li><a href=""><span aria-hidden="true">03</span>Projects</a></li>
+        <li><a href=""><span aria-hidden="true">01</span>ABOUT ME</a></li>
+        <li><a href=""><span aria-hidden="true">02</span>SKILLS</a></li>
+        <li><a href=""><span aria-hidden="true">03</span>HISTORY</a></li>
+        <li><a href=""><span aria-hidden="true">04</span>PROJECTS</a></li>
       </ul>
     </nav>
   </header>
