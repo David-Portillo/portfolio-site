b6f06c74424e602eb86480532c1d4e278d30e38d
David Portillo <david.portillo92@gmail.com>
2024-05-12 14:34:14 -0400

    change sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ const App: Component = () => {
   return (
     <>
       <Header />
+      <div class='title-section'>About Me</div>
       <div class='title-section'>Skills</div>
+      <div class='title-section'>History</div>
+      <div class='title-section'>Projects</div>
     </>
   );
 };
