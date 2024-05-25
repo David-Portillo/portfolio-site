9efa695aabb0fd4c2f1739e726bb2ecc4b0905f7
David Portillo <david.portillo92@gmail.com>
2024-05-25 16:52:17 -0400

    added javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const navLinks = document.querySelectorAll("nav ul li a");
+console.log(navLinks);
+
+navLinks.forEach((item) => {
+  item.addEventListener("click", function () {
+    document.querySelector("nav ul li a.active").classList.remove("active");
+    item.classList.add("active");
+    document.querySelector("main > section.active").classList.remove("active");
+    document
+      .querySelector(`main > section${item.getAttribute("href")}`)
+      .classList.add("active");
+  });
+});
+
+document
+  .querySelector("#sidebar .toggle-sidebar")
+  .addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("open");
+  });
