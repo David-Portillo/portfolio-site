dbc0d8437e2e9ad1c9c6ccc61200be94d247af5c
David Portillo <david.portillo92@gmail.com>
2024-06-15 11:32:01 -0400

    completed about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-const navLinks = document.querySelectorAll("nav ul li a");
+const navLinks = document.querySelectorAll("a.inner-link");
 console.log(navLinks);
 
 navLinks.forEach((item) => {
   item.addEventListener("click", function () {
     document.querySelector("nav ul li a.active").classList.remove("active");
-    item.classList.add("active");
+    document.querySelector(`nav ul li a[href="${item.getAttribute("href")}"]`).classList.add("active");
     document.querySelector("main > section.active").classList.remove("active");
     document
       .querySelector(`main > section${item.getAttribute("href")}`)
